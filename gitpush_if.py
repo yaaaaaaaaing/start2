cdb67df72d12aff9a1e23c35cd5f8b47e116210a
yaaaaaaaaing <liyang.tjtj@gmail.com>
2025-01-17 22:40:03 +0800

    test

--- gitpush_if.py
+++ gitpush_if.py
@@ -47,11 +47,13 @@ def push_database_to_github(change_file_list,name_list_path,hash_json_file,commi
         except:
             continue
     subprocess.call("git status", shell=True, cwd=config_path)
+
+
+    subprocess.call("git submodule sync --recursive", shell=True, cwd=config_path)
+    subprocess.call("git submodule foreach --recursive 'git remote set-url origin git@github.com:yaaaaaaaaing/start2_database.git'", shell=True, cwd=config_path)
     commit = f'git commit -m "{commit_message}"'
     subprocess.call(commit, shell=True,cwd=config_path)
     subprocess.call("git status", shell=True, cwd=config_path)
-    subprocess.call("ssh -T git@github.com", shell=True, cwd=config_path)
-    subprocess.call("git submodule foreach --recursive 'git remote set-url origin git@github.com:yaaaaaaaaing/start2_database.git'", shell=True, cwd=config_path)
     subprocess.call("git push origin HEAD", shell=True,cwd=config_path)
     subprocess.call("git status", shell=True, cwd=config_path)
 
