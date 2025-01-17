c681fdb34b514c10d5e6852d921b6a5faaeff230
yang.li18 <yang.li18@nio.com>
2025-01-17 17:06:52 +0800

    add Test code

--- gitpush_if.py
+++ gitpush_if.py
@@ -50,6 +50,7 @@ def push_database_to_github(change_file_list,name_list_path,hash_json_file,commi
     commit = f'git commit -m "{commit_message}"'
     subprocess.call(commit, shell=True,cwd=config_path)
     subprocess.call("git status", shell=True, cwd=config_path)
+    subprocess.call("git config --get remote.origin.url ", shell=True, cwd=config_path)
     subprocess.call("git push origin HEAD", shell=True,cwd=config_path)
     subprocess.call("git status", shell=True, cwd=config_path)
 
